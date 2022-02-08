c9444c0921f789b18e79c2a457314dc73a9a14a3
Yong Rui <tanyongruifenway@gmail.com>
2022-02-08 21:34:19 +0800

    Added basic movement script. Added placeholder player image.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour {
+    
+    public float playerSpeed;
+    public float sprintSpeed = 4f;
+    public float walkSpeed = 2f;
+    private bool isMoving = false;
+    private bool isSprinting =false;
+    
+    public Animator anim;
+    public Rigidbody rigidBody;
+
+    private Vector3 moveDirection;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        playerSpeed = walkSpeed;
+        // anim = GetComponent<Animator>();
+        // rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        ProcessInputs();
+    }
+
+    // Update is called at a fixed rate
+    void FixedUpdate() {
+        Move();
+    }
+
+    void ProcessInputs() {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = 0;
+        float moveZ = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector3(moveX, moveY, moveZ).normalized;
+    }
+
+    void Move() {
+        rigidBody.velocity = new Vector3(moveDirection.x * playerSpeed, 
+                moveDirection.y * playerSpeed, 
+                moveDirection.z * playerSpeed);
     }
 }
