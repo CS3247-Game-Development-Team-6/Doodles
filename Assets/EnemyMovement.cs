8f64a63b43622a660ec2f0e1267fe6f345243079
Wayne Tan <waynetan2000@gmail.com>
2022-02-14 19:06:45 +0800

    move enemy to the first waypoint

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // first target, which is first waypoint in Waypoints
+        target = Waypoints.points[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // movement direction to the target waypoint
+        Vector3 direction = target.position - transform.position;
+        
+        // delta time is time passed since last frame
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        
+    }
+}
