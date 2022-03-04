5c797e74fafaf570ff6ff56a75c4e845300e35a1
Wayne Tan <waynetan2000@gmail.com>
2022-03-04 19:19:23 +0800

    fix waypoint + change base hp text

--- Assets/Scripts/GameManager/BaseHpUI.cs
+++ Assets/Scripts/GameManager/BaseHpUI.cs
@@ -7,16 +7,10 @@ public class BaseHpUI : MonoBehaviour
 {
     public Text baseHpText;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
         // if the game scales, may need to change implementation using coroutine
-        baseHpText.text = "BASE HP: " + BaseHp.hp.ToString();
+        baseHpText.text = "BASE: " + BaseHp.hp.ToString() + " HP";
     }
 }
