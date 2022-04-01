013647648ee3369ecc43d6f6969c54cd6f4409d8
Timothy Yang <timothyoung97@gmail.com>
2022-04-01 22:04:37 +0800

    Resolve merge conflict

--- Assets/Scripts/Map/Node.cs
+++ Assets/Scripts/Map/Node.cs
@@ -19,7 +19,7 @@ public class Node : MonoBehaviour
 
     private bool isTowerBuilt = false;
     private bool isAddedElement = false;
-    private GameObject tower;
+    public GameObject tower;
     public Cell cell;
 
     public Vector3 tileOffset = Vector3.zero;
