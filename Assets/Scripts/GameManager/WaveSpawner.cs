1951fed0ef29ffe50eca16ad13a1cfd336bea799
Wayne Tan <waynetan2000@gmail.com>
2022-04-05 19:30:14 +0800

    add enemies image

--- Assets/Scripts/GameManager/WaveSpawner.cs
+++ Assets/Scripts/GameManager/WaveSpawner.cs
@@ -37,7 +37,7 @@ public class WaveSpawner : MonoBehaviour
     void Update()
     {
 
-        enemiesLeftText.text = "Enemies left: " + string.Format("{0}", numEnemiesLeftInWave);
+        enemiesLeftText.text = ": " + string.Format("{0}", numEnemiesLeftInWave);
 
         if (numEnemiesAlive > 0 || isSpawningEnemy)
         {
