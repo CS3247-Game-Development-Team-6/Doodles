5d62ac55068abee335a36286769f7522e4be3f6f
Wayne Tan <waynetan2000@gmail.com>
2022-08-09 14:08:36 +0800

    code style: enemy.cs

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,12 +1,9 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class Enemy : MonoBehaviour
-{
+public class Enemy : MonoBehaviour {
 
     [SerializeField] private float speed;               // Serialized for debugging purposes
 
@@ -35,7 +32,7 @@ public class Enemy : MonoBehaviour
     private bool isScalded = false;
     private bool isFrozen = false;
     private bool isWeakened = false;
-   
+
     public bool isInFog = true;
     //private MeshRenderer ballMeshRenderer;
     private Transform ballParentTransform;
@@ -45,14 +42,14 @@ public class Enemy : MonoBehaviour
 
     private int lastXCoord = 0;
     private int lastYCoord = 0;
-    
+
     // to reduce bullet damage
     private GameObject bulletPrefab;
 
     [Header("Unity Stuff")]
     public Image healthBar;
     public TMP_Text healthText;
-    
+
     public MapGenerator map;
     private Cell[,] cells;
 
@@ -64,11 +61,9 @@ public class Enemy : MonoBehaviour
         inkManager = InkManager.instance;
     }
 
-    public void TakeDamage(float amount)
-    {
+    public void TakeDamage(float amount) {
         // Damage to be taken is higher than defense
-        if (defense < amount)
-        {
+        if (defense < amount) {
             health = health - amount + defense;
         }
 
@@ -76,8 +71,7 @@ public class Enemy : MonoBehaviour
         healthBar.fillAmount = health / initHealth;
     }
 
-    public void TakeDot(float amount)
-    {
+    public void TakeDot(float amount) {
         health = health - amount;
 
         // float number between 0 and 1
@@ -85,119 +79,98 @@ public class Enemy : MonoBehaviour
 
     }
 
-    public void ReduceSpeed(float slowAmount)
-    {
+    public void ReduceSpeed(float slowAmount) {
         speed = initSpeed * slowAmount;
     }
 
-    public void RestoreSpeed()
-    {
+    public void RestoreSpeed() {
         speed = initSpeed;
     }
 
-    public void ReduceAttack(int atkDecreAmount)
-    {
+    public void ReduceAttack(int atkDecreAmount) {
         bulletPrefab.GetComponent<EnemyBullet>().ReduceBulletDamage(atkDecreAmount);
     }
 
-    public void RestoreAttack()
-    {
+    public void RestoreAttack() {
         bulletPrefab.GetComponent<EnemyBullet>().RestoreBulletDamage();
     }
 
-    public void ReduceDefense(int defDecreAmount)
-    {
+    public void ReduceDefense(int defDecreAmount) {
         if (defDecreAmount > initDefense) defense = 0;
         else defense = initDefense - defDecreAmount;
     }
 
-    public void RestoreDefense()
-    {
+    public void RestoreDefense() {
         defense = initDefense;
     }
 
-    public void setScorched(bool b)
-    {
+    public void setScorched(bool b) {
         isScorched = b;
     }
 
-    public bool getScorched()
-    {
+    public bool getScorched() {
         return isScorched;
     }
 
-    public void setChilled(bool b)
-    {
+    public void setChilled(bool b) {
         isChilled = b;
     }
 
-    public bool getChilled()
-    {
+    public bool getChilled() {
         return isChilled;
     }
 
-    public void setDrenched(bool b)
-    {
+    public void setDrenched(bool b) {
         isDrenched = b;
     }
 
-    public bool getDrenched()
-    {
+    public bool getDrenched() {
         return isDrenched;
     }
 
-    public void setScalded(bool b)
-    {
+    public void setScalded(bool b) {
         isScalded = b;
     }
 
-    public bool getScalded()
-    {
+    public bool getScalded() {
         return isScalded;
     }
 
-    public void setFrozen(bool b)
-    {
+    public void setFrozen(bool b) {
         isFrozen = b;
     }
 
-    public bool getFrozen()
-    {
+    public bool getFrozen() {
         return isFrozen;
     }
 
-    public void setWeakened(bool b)
-    {
+    public void setWeakened(bool b) {
         isWeakened = b;
     }
 
-    public bool getWeakened()
-    {
+    public bool getWeakened() {
         return isWeakened;
     }
 
-    public bool getInFog()
-    {
+    public bool getInFog() {
         return isInFog;
     }
 
-    void Die ()
-    {
+    void Die() {
         // add ink
         inkManager.ChangeInkAmount(inkGained);
 
         // for new wave
         WaveSpawner.numEnemiesAlive--;
         WaveSpawner.numEnemiesLeftInWave--;
-        
+
         GameObject effect = Instantiate(deathEffect, transform.position, Quaternion.identity);
         Destroy(effect, 5f);
         Destroy(gameObject);
-  
+
     }
 
-    void Start()
-    {
+    void Start() {
         // Initialize Stats
         health = initHealth;
         speed = initSpeed;
@@ -214,18 +187,16 @@ public class Enemy : MonoBehaviour
 
         // first target, which is first waypoint in Waypoints
         target = Waypoints.points[0];
-        
+
         // get a reference to all cells for checking if a tile is fogged or not
         cells = GameObject.Find("Map").GetComponent<MapGenerator>().GetCells();
     }
 
 
-    void Update()
-    {
+    void Update() {
         healthText.text = string.Format("{0}", health);
 
-        if (health <= 0)
-        {
+        if (health <= 0) {
             Die();
         }
 
@@ -239,8 +210,7 @@ public class Enemy : MonoBehaviour
             return;
         }
 
-        if (isFrozen)
-        {
+        if (isFrozen) {
             // stop movement
             animator.SetBool("isWalking", false);
             return;
@@ -251,49 +221,43 @@ public class Enemy : MonoBehaviour
         // movement direction to the target waypoint
         Vector3 direction = target.position - transform.position;
 
-        if (direction != Vector3.zero)
-        {
+        if (direction != Vector3.zero) {
             // Do the rotation here
             Quaternion lookAtRotation = Quaternion.LookRotation(direction);
             Vector3 rotation = Quaternion.Lerp(model.transform.rotation, lookAtRotation, Time.deltaTime * 10f).eulerAngles;
             model.transform.rotation = Quaternion.Euler(rotation.x, rotation.y, rotation.z);
-        }        
+        }
 
         // delta time is time passed since last frame
         transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
-        
+
         var currentPosition = transform.position;
 
-        if (Vector3.Distance(currentPosition, target.position) <= 0.2f)
-        {
+        if (Vector3.Distance(currentPosition, target.position) <= 0.2f) {
             GetNextWaypoint();
         }
 
         int currentXCoord = Convert.ToInt32(Math.Floor(currentPosition.x));
         int currentYCoord = Convert.ToInt32(Math.Floor(currentPosition.z));
 
-        if (currentXCoord != lastXCoord || currentYCoord != lastYCoord)
-        {
+        if (currentXCoord != lastXCoord || currentYCoord != lastYCoord) {
             lastXCoord = currentXCoord;
             lastYCoord = currentYCoord;
             isInFog = GetCurrentTileFogged(currentXCoord, currentYCoord);
         }
-        
+
         // enemy is visible if not in fog, hence its visibility is the negation of the isInFog bool.
         setEnemyVisibility(!isInFog);
-        
+
     }
 
-    private bool GetCurrentTileFogged(int xCoord, int yCoord)
-    {
+    private bool GetCurrentTileFogged(int xCoord, int yCoord) {
         Cell cell = cells[yCoord, xCoord];
         return cell.isFog;
     }
 
-    void GetNextWaypoint()
-    {
-        if (waypointIndex >= Waypoints.points.Length - 1) 
-        {
+    void GetNextWaypoint() {
+        if (waypointIndex >= Waypoints.points.Length - 1) {
             EndPath();
             return;
         }
@@ -302,30 +266,26 @@ public class Enemy : MonoBehaviour
     }
 
     // previously was enemy attack the base and destroy
-    void EndPath()
-    {
+    void EndPath() {
         // do nothing
         animator.SetBool("isWalking", false);
     }
 
-    private void setEnemyVisibility(bool isVisible)
-    {
+    private void setEnemyVisibility(bool isVisible) {
         //ballMeshRenderer.enabled = isVisible;
-        foreach (Transform childrenTransform in ballParentTransform)
-        {
+        foreach (Transform childrenTransform in ballParentTransform) {
             // disable canvas
             if (childrenTransform.name == canvas.name)
                 childrenTransform.gameObject.SetActive(isVisible);
 
             // disable every renderers
-            if (childrenTransform.name == model.name)
-            {
+            if (childrenTransform.name == model.name) {
                 SkinnedMeshRenderer[] renderers = model.GetComponentsInChildren<SkinnedMeshRenderer>();
                 foreach (SkinnedMeshRenderer r in renderers) {
                     r.enabled = isVisible;
                 }
             }
-              
+
         }
     }
 
