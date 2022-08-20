c89618863c0b822f566e68571892997aa4535a8b
Wayne Tan <waynetan2000@gmail.com>
2022-08-20 23:36:39 +0800

    add enemyinfo script and enemyfast scriptableobjects

---
+++ Assets/Scripts/Enemy/EnemyInfo.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Enemy Info", menuName = "Enemy Info")]
+public class EnemyInfo : ScriptableObject {
+    public float speed;
+    public float health;
+    public int defense;
+    public float inkGained;
+    public GameObject deathEffect;
+}
