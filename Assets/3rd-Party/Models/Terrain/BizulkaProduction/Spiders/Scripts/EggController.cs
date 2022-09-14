1eec13e7d0cd9aae313267efcdb37327cfc85036
Wayne Tan <58804560+tanwayne890@users.noreply.github.com>
2022-09-15 01:06:27 +0800

    Add spiders (#208)
    * add elementEffectType
    
    * Add elementType into enemyInfo (scriptable object), recalculate damage
    
    * use example enemy: elementalMelee
    
    * add all spiders and debug log
    
    * add damagetext when enemy receive damage
    
    * Add `SpiderScene-prototype` (#211)
    
    * add cemetery halloween set
    
    * add prototype scene with spider bg, base and necessary albedo
    
    * remove cemetery halloween set (due to high poly and colourful)
    
    * add deadtrees, bones, skull, eggs
    
    * add spider animator, spider models and prefabs
    
    * Spider model fix
    
    - Fix textures for spider models
    - Fix Polygon face orientation for spider models
    
    * gather all spider prefabs into one folder
    
    * move materials and fbms into Assets/Materials/Enemy/Spiders
    
    * add spider level info for testing, fix FireMeleeSpider name, adjust lighting
    
    * remove bizulkaproduction sample scene
    
    * remove explosiveLLC
    
    Co-authored-by: Danh Nguyen <ducdanh24112001@gmail.com>
    Co-authored-by: trxe <peixianwong@gmail.com>
    
    * change damageText colour green
    
    * add elementInfo into EnemyInfo, add weakness into elementInfo
    
    * add damage multiplier into EnemyInfo
    
    Co-authored-by: Danh Nguyen <ducdanh24112001@gmail.com>
    Co-authored-by: trxe <peixianwong@gmail.com>

---
+++ Assets/3rd-Party/Models/Terrain/BizulkaProduction/Spiders/Scripts/EggController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class EggController : MonoBehaviour
+{
+    [SerializeField] GameObject _full;
+    [SerializeField] GameObject _damaged;
+    [SerializeField] private ParticleSystem _particleSystem;
+
+    void OnMouseOver()
+    {
+        Destroy();
+    }
+
+    public void Clear()
+    {
+        _full.gameObject.SetActive(true);
+        _damaged.gameObject.SetActive(false); 
+    }
+    public void Destroy()
+    {
+        _full.gameObject.SetActive(false);
+        _damaged.gameObject.SetActive(true);
+        _particleSystem.Play();
+    }
+
+}
