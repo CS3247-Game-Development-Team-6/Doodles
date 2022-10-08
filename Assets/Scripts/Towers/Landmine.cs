a680465f89ac22815743fec3569df02709b54df6
Timothy Young <timothyoung97@gmail.com>
2022-10-08 21:07:42 +0800

    Update Landmine.cs

--- Assets/Scripts/Towers/Landmine.cs
+++ Assets/Scripts/Towers/Landmine.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
 
 public class Landmine : Tower {
+    private Collider[] enemyInRange;
+    private const bool PENETRATE_TARGET = false;
+    private const int ENEMY_LAYER = 6;
     private Transform rotationBase;
     private Transform firePoint;
+
 
     public override void SetTowerInfo(TowerInfo towerInfo) {
         base.SetTowerInfo(towerInfo);
@@ -11,11 +15,56 @@ public class Landmine : Tower {
     }
 
     public void Start() {
-        
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+        enemyInRange = Physics.OverlapSphere(transform.position, range, ENEMY_LAYER);
+    }
+
+    public void UpdateTarget() {
+        enemyInRange = Physics.OverlapSphere(transform.position, range, ENEMY_LAYER);
+    }
+
+    public override void Shoot()
+    {
+        base.Shoot();
+        foreach (var enemy in enemyInRange)
+        {
+            print(enemy.name);
+            if (enemy.gameObject.tag == ENEMY_TAG) {
+                GameObject bulletObj = (GameObject) Instantiate (
+                    bulletPrefab, 
+                    enemy.transform.position, 
+                    enemy.transform.rotation
+                );
+
+                Bullet bullet = bulletObj.GetComponent<Bullet>();
+                bullet.SetBulletInfo(towerInfo);
+                float distance = Vector3.Distance(transform.position, enemy.transform.position);
+                bullet.SetBulletDamage(calculateDamage(distance));
+                bullet.Seek(enemy.transform, PENETRATE_TARGET);
+            }
+        }
+        // TODO: Create tower self destruction effect
+        // TODO: Create tower self destruction sound
+        Destroy(gameObject);
+    }
+
+    private float calculateDamage(float distance) {
+        float damage = Mathf.Cos((1/(range/3) * distance)) + 1; 
+        return damage;
     }
 
     // Update is called once per frame
     public override void Update() {
-        
+        base.Update();
+
+        if (health <= 0) return;
+        if (enemyInRange.Length == 0) return;
+
+        // TODO: Create trigger for enemy when stepped on landmine
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, 3);
     }
 }
