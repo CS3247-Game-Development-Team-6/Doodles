44602587427afddb2604004d2e195eec97da7985
Timothy Young <timothyoung97@gmail.com>
2022-10-09 16:31:45 +0800

    Comment Landmine.cs

--- Assets/Scripts/Towers/Landmine.cs
+++ Assets/Scripts/Towers/Landmine.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 
 public class Landmine : Tower {
+    
+    // List that holds all the enemies in range
     private Collider[] enemyInRange;
     private const bool PENETRATE_TARGET = false;
     public const string VFX_NAME = "VFX";
-
     private Transform rotationBase;
     private Transform firePoint;
+
+    [Header("Effect Prefabs")]
     public GameObject selfDestructEffect;
     public GameObject selfDestructSound;
 
@@ -20,6 +23,7 @@ public class Landmine : Tower {
         InvokeRepeating("UpdateTarget", 0f, 0.5f);
     }
 
+    // Find all objects in range
     public void UpdateTarget() {
         enemyInRange = Physics.OverlapSphere(transform.position, range);
     }
@@ -47,6 +51,7 @@ public class Landmine : Tower {
         Destroy(gameObject, 1f);
     }
 
+    // Trigger visual and sound effects
     private void TriggerEffect() {
         GameObject selfDestructEffectParticle = (GameObject)Instantiate(selfDestructEffect, transform.position, transform.rotation);
         selfDestructEffectParticle.transform.parent = transform;
@@ -55,26 +60,27 @@ public class Landmine : Tower {
         selfDestructEffectAudio.GetComponent<AudioSource>().Play();
     }
 
+    // Calculate damage based on distance of the enemy from the origin of the landmine orb
     private float calculateDamage(float distance) {
         float cosDamage = Mathf.Cos((1/(range/3) * distance)) + 1; 
         return towerInfo.damage * cosDamage;
     }
 
-    // Update is called once per frame
     public override void Update() {
+        // Create a overlap sphere to detect enemy that touches the landmine orbs
         Collider[] enemyTrigger = Physics.OverlapSphere(firePoint.position, 0.1f);
-        foreach (var enemy in enemyTrigger)
-        {
+        foreach (var enemy in enemyTrigger){
             if (enemy.gameObject.tag == ENEMY_TAG) {
                 Shoot();
             }
         }
     }
 
-    private void OnDrawGizmosSelected() {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, 3);
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(firePoint.position, 0.1f);
-    }
+    // Debug Tool: Draw a sphere to show the range of the tower
+    // private void OnDrawGizmosSelected() {
+    //     Gizmos.color = Color.red;
+    //     Gizmos.DrawWireSphere(transform.position, 3);
+    //     Gizmos.color = Color.yellow;
+    //     Gizmos.DrawWireSphere(firePoint.position, 0.1f);
+    // }
 }
