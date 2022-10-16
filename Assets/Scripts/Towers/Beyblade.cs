0668319e11f30500a095320867664b4164e5d6a9
Timothy Young <timothyoung97@gmail.com>
2022-10-16 13:12:33 +0800

    Add spinning mechanism

--- Assets/Scripts/Towers/Beyblade.cs
+++ Assets/Scripts/Towers/Beyblade.cs
@@ -1,19 +1,12 @@
 using UnityEngine;
 
 public class Beyblade : Tower {
-    
-    // List that holds all the enemies in range
     private Collider[] enemyInRange;
-    private Collider[] enemyTrigger;
     private const bool PENETRATE_TARGET = false;
-    private bool isExploded = false;
+    private bool isSpinning = false;
     public const string VFX_NAME = "VFX";
     private Transform rotationBase;
     private Transform firePoint;
-
-    [Header("Effect Prefabs")]
-    public GameObject selfDestructEffect;
-    public GameObject selfDestructSound;
 
     public override void SetTowerInfo(TowerInfo towerInfo) {
         base.SetTowerInfo(towerInfo);
@@ -26,65 +19,24 @@ public class Beyblade : Tower {
     }
 
     public void UpdateTarget() {
-        // A list of objects contained in a sphere of radius = towerInfo.range, centered at the tower's origin
         enemyInRange = Physics.OverlapSphere(transform.position, range);
-        // A list of objects contained in a sphere of radius = 0.1f, centered at the tower's orb
-        enemyTrigger = Physics.OverlapSphere(firePoint.position, 0.1f);
-    }
-
-    public override void Shoot() {
-        base.Shoot();
-        foreach (var enemy in enemyInRange) {
-            if (enemy.gameObject.tag == ENEMY_TAG) {
-                GameObject bulletObj = (GameObject) Instantiate (
-                    bulletPrefab, 
-                    enemy.transform.position, 
-                    enemy.transform.rotation
-                );
-
-                Bullet bullet = bulletObj.GetComponent<Bullet>();
-                bullet.SetBulletInfo(towerInfo);
-                float distance = Vector3.Distance(transform.position, enemy.transform.position);
-                bullet.SetBulletDamage(CalculateDamage(distance));
-                bullet.Seek(enemy.transform, PENETRATE_TARGET);
-            }
-        }
-        TriggerEffect();
-        Destroy(gameObject, 0.5f);
-    }
-
-    // Trigger visual and sound effects
-    private void TriggerEffect() {
-        GameObject selfDestructEffectParticle = (GameObject)Instantiate(selfDestructEffect, transform.position, transform.rotation);
-        selfDestructEffectParticle.transform.parent = transform;
-        GameObject selfDestructEffectAudio = (GameObject)Instantiate(selfDestructSound, transform.position, transform.rotation);
-        selfDestructEffectAudio.transform.SetParent(transform);
-        selfDestructEffectAudio.GetComponent<AudioSource>().Play();
-    }
-
-    // Calculate damage based on distance of the enemy from the origin of the landmine orb
-    private int CalculateDamage(float distance) {
-        float cosDamage = .5f * Mathf.Cos((1/(0.4f*range) * distance)) + .5f; 
-        return Mathf.FloorToInt(towerInfo.damage * cosDamage);
     }
 
     public override void Update() {
-        // Checks if an enemy has entered the trigger and if the landmine has not exploded yet
-        if (enemyTrigger != null && !isExploded) {
-            foreach (var enemy in enemyTrigger) {
+        // check if enemyInRange contains gameobject tagged as enemy
+        if (enemyInRange != null) {
+            foreach (var enemy in enemyInRange) {
                 if (enemy.gameObject.tag == ENEMY_TAG) {
-                    Shoot();
-                    isExploded = true;
+                    isSpinning = true;
+                    break;
                 }
+                isSpinning = false;
             }
+        }
+
+        if (isSpinning) {
+            rotationBase.Rotate(0f, 360f * Time.deltaTime, 0f);
         }
     }
 
-    // Debug Tool: Draw a sphere to show the range of the tower
-    // private void OnDrawGizmosSelected() {
-    //     Gizmos.color = Color.red;
-    //     Gizmos.DrawWireSphere(transform.position, 3);
-    //     Gizmos.color = Color.yellow;
-    //     Gizmos.DrawWireSphere(firePoint.position, 0.1f);
-    // }
 }
