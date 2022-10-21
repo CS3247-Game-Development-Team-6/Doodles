7530c1e8a8e9e04d2378b53954c75e7bf9e711d0
Wayne Tan <58804560+tanwayne890@users.noreply.github.com>
2022-10-21 23:53:39 +0800

    Fix buttons on WinScreen and GameOverScreen (#309)
    * win screen next button
    
    * finalize
    
    * change next to menu

--- Assets/Scripts/GameManager/GameStateManager.cs
+++ Assets/Scripts/GameManager/GameStateManager.cs
@@ -1,10 +1,8 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class GameStateManager : MonoBehaviour {
     [SerializeField] private GameObject gameOverUI;
     [SerializeField] private GameObject winUI;
-    public string nextSceneName;
 
     private static bool isGameEnded;
     private Map map;
@@ -12,13 +10,6 @@ public class GameStateManager : MonoBehaviour {
     void Start() {
         isGameEnded = false;
         map = FindObjectOfType<Map>();
-
-        /*
-        GameObject[] ddols = GetDontDestroyOnLoadObjects();
-        for (int i = 0; i < ddols.Length; i++) {
-            ddols[i].transform.Find(LoadingUI.loadingUIName).gameObject.SetActive(false);
-        }
-        */
     }
 
     void Update() {
@@ -32,27 +23,12 @@ public class GameStateManager : MonoBehaviour {
 
     public void EndGame() {
         isGameEnded = true;
-
         gameOverUI.SetActive(true);
     }
 
     public void WinGame() {
-        GameObject[] ddols = GetDontDestroyOnLoadObjects();
-        for (int i = 0; i < ddols.Length; i++) {
-            ddols[i].SetActive(false);
-        }
-
         isGameEnded = true;
-
         winUI.SetActive(true);
-    }
-
-    public void GotoNextScene() {
-        GameObject[] ddols = GetDontDestroyOnLoadObjects();
-        for (int i = 0; i < ddols.Length; i++) {
-            Destroy(ddols[i]);
-        }
-        SceneManager.LoadScene(nextSceneName);
     }
 
     // for other game scripts to check if the game is ended
@@ -75,4 +51,5 @@ public class GameStateManager : MonoBehaviour {
                 Object.DestroyImmediate(temp);
         }
     }
+
 }
