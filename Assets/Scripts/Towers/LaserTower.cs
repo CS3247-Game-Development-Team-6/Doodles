87b9cf84c6a5c3fe595be843438ebb288b4c2d48
Timothy Young <timothyoung97@gmail.com>
2022-10-30 16:18:48 +0800

    Add AFX only to Laser Tower

--- Assets/Scripts/Towers/LaserTower.cs
+++ Assets/Scripts/Towers/LaserTower.cs
@@ -20,6 +20,11 @@ public class LaserTower : Tower {
     private GameObject impactEffect;
     private Light impactLight;
 
+    [Header("Effect Prefabs")]
+    public GameObject fireSoundEffect;
+    private float fireSoundEffectCurrTimeCount = 4;
+    private float fireSoundEffectTriggerDuration = 5;
+
     public override void SetTowerInfo(TowerInfo towerInfo) {
         base.SetTowerInfo(towerInfo);
         rotationBase = transform.Find(Tower.ROTATION_BASE_NAME);
@@ -86,6 +91,15 @@ public class LaserTower : Tower {
 
         // decrease health
         base.DecreaseHealth(Time.deltaTime);
+
+        // laser sound effect
+        fireSoundEffectCurrTimeCount += Time.deltaTime;
+        if (fireSoundEffectCurrTimeCount >= fireSoundEffectTriggerDuration) {
+            fireSoundEffectCurrTimeCount = 0;
+            GameObject fireSoundEffectPrefab = (GameObject)Instantiate(fireSoundEffect, firePoint.position, firePoint.rotation);
+            fireSoundEffectPrefab.transform.SetParent(transform);
+            Destroy(fireSoundEffectPrefab, 5f);
+        }
 
         // laser graphics
         if (!lineRenderer.enabled) {
