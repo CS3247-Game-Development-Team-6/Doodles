a93c64d9254c18e6fb465635e7be00ca5c065cbe
Chen Hao <chenhaobnds84@gmail.com>
2022-11-01 13:43:32 +0800

    change count down

--- Assets/Scripts/UI/Loadout/GameStateInfoUI.cs
+++ Assets/Scripts/UI/Loadout/GameStateInfoUI.cs
@@ -2,31 +2,34 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameStateInfoUI : MonoBehaviour {
+public class GameStateInfoUI : MonoBehaviour
+{
     public MapInfo mapInfo;
     public Image inkSlider;
     public TextMeshProUGUI inkText;
     public Image enemySlider;
     public TextMeshProUGUI enemyText;
-    public Image countdownSlider;
-
+    [SerializeField] private TMP_Text textCountDown;
     private InkManager inkManager;
     private ChunkSpawner chunkSpawner;
     private int totalChunks;
     private int totalWaves;
 
-    public void SetInkManager(InkManager inkManager) {
+    public void SetInkManager(InkManager inkManager)
+    {
         this.inkManager = inkManager;
     }
 
-    public void SetChunkSpawner(ChunkSpawner chunkSpawner) {
+    public void SetChunkSpawner(ChunkSpawner chunkSpawner)
+    {
         this.chunkSpawner = chunkSpawner;
     }
 
-    private void Start() {
+    private void Start()
+    {
         mapInfo = Loadout.mapToLoad;
-
-        if (!mapInfo) {
+        if (!mapInfo)
+        {
             Debug.Log("No map loaded.");
             return;
         }
@@ -36,36 +39,64 @@ public class GameStateInfoUI : MonoBehaviour {
         inkText.text = $"{startingInk} / {mapInfo.totalInk}";
         enemySlider.fillAmount = 0f;
         totalWaves = 0;
-        foreach (var chunk in mapInfo.chunkInfo) {
+        foreach (var chunk in mapInfo.chunkInfo)
+        {
             totalWaves += chunk.waves.Length;
         }
         totalChunks = mapInfo.chunkInfo.Length;
         enemyText.text = $"{mapInfo.chunkInfo.Length} Levels\n{totalWaves} Total Waves";
+        textCountDown.gameObject.SetActive(false);
+
+
+
     }
 
-    private string GetWaveStatus() {
+    private string GetWaveStatus()
+    {
         int chunkNum = chunkSpawner.chunkIndex;
         int waveNum = chunkSpawner.WavesStarted;
         int totalWavesInChunk = chunkSpawner.waves.Length;
         return $"\nWave {waveNum} / {totalWavesInChunk}\nin level {chunkNum} / {totalChunks}";
     }
 
-    private float GetChunkCompletion() {
+    private float GetChunkCompletion()
+    {
         float waveNum = chunkSpawner.waveIndex;
         float totalWavesInChunk = chunkSpawner.waves.Length;
         return totalWavesInChunk == 0 ? 0 : waveNum / totalWavesInChunk;
     }
 
-    private void Update() {
-        if (inkManager != null) {
+    private void Update()
+    {
+        if (inkManager != null)
+        {
             inkSlider.fillAmount = inkManager.InkFraction;
             inkText.text = inkManager.InkString;
         }
 
-        if (chunkSpawner != null) {
+        if (chunkSpawner != null)
+        {
+            if (Mathf.RoundToInt(chunkSpawner.countdownTimer) > 0 & Mathf.RoundToInt(chunkSpawner.countdownTimer) != Mathf.RoundToInt(chunkSpawner.timeBetweenWaves))
+            {
+
+                textCountDown.text = Mathf.RoundToInt(chunkSpawner.countdownTimer).ToString();
+                textCountDown.gameObject.SetActive(true);
+
+
+
+
+            }
+            else if (Mathf.RoundToInt(chunkSpawner.countdownTimer) == 0 | chunkSpawner.countdownTimer == chunkSpawner.timeBetweenWaves)
+            {
+              
+                textCountDown.gameObject.SetActive(false);
+
+
+            }
+
             enemyText.text = GetWaveStatus();
             enemySlider.fillAmount = GetChunkCompletion();
-            countdownSlider.fillAmount = chunkSpawner.countdownTimer / chunkSpawner.timeBetweenWaves;
+
         }
     }
 
